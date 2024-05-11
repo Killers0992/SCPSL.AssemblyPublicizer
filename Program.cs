75a41c70b6230852bd20883196c2c679d17b5e8d
Killers0992 <38152961+Killers0992@users.noreply.github.com>
2024-05-11 15:50:03 +0200

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,46 @@
+﻿using BepInEx.AssemblyPublicizer;
+using McMaster.Extensions.CommandLineUtils;
+using Microsoft.Extensions.Hosting;
+using System.ComponentModel.DataAnnotations;
+
+await Host.CreateDefaultBuilder()
+    .RunCommandLineApplicationAsync<AppCommand>(args);
+
+[Command(Description = "Runs assembly publicizer.")]
+public class AppCommand
+{
+    [Required]
+    [Option(Description = "Assemblies to publicize.")]
+    public string Assemblies { get; set; } = null;
+
+    public string SLReferencesPath => Environment.GetEnvironmentVariable("SL_REFERENCES");
+
+    public async Task<int> OnExecute(IConsole console)
+    {
+        try
+        {
+            string[] assembliesToPublicize = Assemblies.Split(",");
+
+            List<string> publicized = new List<string>();
+
+            foreach(var assemblyName in assembliesToPublicize)
+            {
+                string targetPath = Path.Combine(SLReferencesPath, assemblyName);
+
+                if (!File.Exists(targetPath)) continue;
+
+                string publicizePath = Path.Combine(SLReferencesPath, Path.GetFileName(assemblyName) + "-publicized.dll");
+
+                AssemblyPublicizer.Publicize(targetPath, publicizePath);
+
+                Console.WriteLine("Publicized " + assemblyName);
+            }
+            return 0;
+        }
+        catch (Exception ex)
+        {
+            console.WriteLine(ex);
+            return 1;
+        }
+    }
+}
